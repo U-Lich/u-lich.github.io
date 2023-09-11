d44a019b774b0e75cd575b166015640dad15fb64
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-09-11 22:14:03 +0700

    progression

--- src/components/converter/progress-indicator/ProgressIndicator.tsx
+++ src/components/converter/progress-indicator/ProgressIndicator.tsx
@@ -1,4 +1,12 @@
+import ConversionState from "@/objects/conversion/state/ConversionState";
 import useConversion from "../../../hooks/useConversion";
+
+const StateToPercent: { [key in ConversionState]: number } = {
+  [ConversionState.OBJECT]: 0,
+  [ConversionState.FORMAT]: 30,
+  [ConversionState.DECORATION]: 70,
+  [ConversionState.SYNCED]: 100,
+};
 
 export default function ProgressIndicator() {
   const { conversion } = useConversion();
@@ -8,8 +16,17 @@ export default function ProgressIndicator() {
   }
 
   return (
-    <div className="font-bold">
-      <span>{`Current step: <${conversion.state}>`}</span>
+    <div
+      className={`relative h-2 w-2/3 overflow-hidden rounded-full bg-gray-200 transition-opacity ${
+        conversion.state === "OBJECT" ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div
+        className="absolute left-0 top-1/2 h-full w-full -translate-y-1/2 bg-blue-400 transition-all duration-500"
+        style={{
+          width: `${StateToPercent[conversion.state]}%`,
+        }}
+      />
     </div>
   );
 }
